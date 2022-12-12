4f67d2dbdf05637517a011811f7900954c06ba7e
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-12-12 12:56:36 +0530

    ds

---
+++ src/test/java/com/example/NestDigitalApp_backendd/NestDigitalAppBackenddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestDigitalApp_backendd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalAppBackenddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
